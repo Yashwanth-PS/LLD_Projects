c842171a102225ad169e998549e86037ea25df13
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-02 22:24:14 +0530

    Pen Project - Added the Attributes for Pen Class

--- src/model/Pen.java
+++ src/model/Pen.java
@@ -1,4 +1,10 @@
 package model;
 
-public class Pen {
+public class Pen { // Model: Pen classes
+    private long id;
+    private String name;
+    private String brand;
+    private double price;
+    private PenType type;
+    private ClosingType closingType;
 }
