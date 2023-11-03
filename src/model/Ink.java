3d7ce075bb7cd98b9c6249b019b68a7e3dec8966
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-03 22:29:34 +0530

    Pen - Added Getters in Ink Class

--- src/model/Ink.java
+++ src/model/Ink.java
@@ -3,8 +3,18 @@ package model;
 public class Ink { // Model: Data classes
     private InkColor color;
     private InkFeatures features;
+
     public Ink(InkColor color, InkFeatures features) {
         this.color = color;
         this.features = features;
     }
+
+    public InkColor getColor() {
+        return color;
+    }
+
+    public InkFeatures getFeatures() {
+        return features;
+    }
+
 }
