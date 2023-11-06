3ef767ebd588080e3ad071d646e57f2ad7bf0af9
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-06 22:55:14 +0530

    Pen - Implemented Ball Pen Class

---
+++ src/model/BallPen.java
@@ -0,0 +1,21 @@
+package model;
+
+public class BallPen extends Pen implements Refillable {
+    private Refill refill;
+
+    public BallPen(long id, String name, String brand, double price, ClosingType closingType, Refill refill) {
+        super(id, name, brand, price, PenType.BALL, closingType);
+        this.refill = refill;
+    }
+
+    @Override
+    public void write() {
+        System.out.println("Writing with " + getBrand() + " " + getName() + " Ball Pen using " + refill.getInk().getColor() + " ink.");
+    }
+
+    @Override
+    public void refill(Refill refill) {
+        this.refill = refill;
+        System.out.println("Refilling the " + getBrand() + " " + getName() + " Ball Pen with a new refill.");
+    }
+}
