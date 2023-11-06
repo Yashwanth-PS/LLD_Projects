5ceebd7e7eb3ee2710a10685ace652163dadd97d
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-06 22:56:55 +0530

    Pen - Implemented Gel Pen Class

---
+++ src/model/GelPen.java
@@ -0,0 +1,21 @@
+package model;
+
+public class GelPen extends Pen implements Refillable {
+    private Refill refill;
+
+    public GelPen(long id, String name, String brand, double price, ClosingType closingType, Refill refill) {
+        super(id, name, brand, price, PenType.GEL, closingType);
+        this.refill = refill;
+    }
+
+    @Override
+    public void write() {
+        System.out.println("Writing with " + getBrand() + " " + getName() + " Gel Pen using " + refill.getInk().getColor() + " ink.");
+    }
+
+    @Override
+    public void refill(Refill refill) {
+        this.refill = refill;
+        System.out.println("Changing the refill of the  " + getBrand() + " " + getName() + " Gel Pen with a new refill.");
+    }
+}
