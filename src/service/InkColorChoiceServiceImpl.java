f92550fb581d80deaa4d134042376e802d87cce3
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-08 23:58:01 +0530

    Pen - Implemented the Color Choice Service Implementation

---
+++ src/service/InkColorChoiceServiceImpl.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.InkColor;
+
+import java.util.Scanner;
+
+class InkColorChoiceServiceImpl implements InkColorChoiceService {
+    @Override
+    public InkColor chooseInkColor() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose an ink color:");
+        for (int i = 0; i < InkColor.values().length; i++) {
+            System.out.println((i + 1) + ". " + InkColor.values()[i]);
+        }
+
+        int inkColorChoice = scanner.nextInt();
+        return InkColor.values()[inkColorChoice - 1];
+    }
+}
